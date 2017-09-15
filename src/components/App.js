47b8ec7f5068d39d11772409ebd57e31a5e21325
Shane Johnson <shane.johnson@gamesys.co.uk>
2017-09-15 10:58:19 +0100

    replacing search with replaceState

--- src/components/App.js
+++ src/components/App.js
@@ -7,51 +7,51 @@ class App extends React.Component {// this is the root of the app
 		super(props);
 //this varable will be used across this component
 		// this.text = '';
-    //empty string this is not a state so the render function will not change
+	//empty string this is not a state so the render function will not change
 		this.state = {
-      selected: null,
-      text: ''
-    };
-    //similar to a variable two differences can't change the state with an "=" instead you MUST "setState"
-    //each time setState is called and the state is changed react rerenders the compenent(calls render function again)
+		selected: null,
+		text: ''
+	};
+	//similar to a variable two differences can't change the state with an "=" instead you MUST "setState"
+	//each time setState is called and the state is changed react rerenders the compenent(calls render function again)
 
 		this._textChanged = this._textChanged.bind(this);
 		this._buttonClicked = this._buttonClicked.bind(this);
 	}
 
-  componentWillMount(){//if this is defined it will automatically be called for you this will be called automatically after the render function is triggered for the first time
-    const urlParams = new URLSearchParams(window.location.search);
+	componentWillMount(){//if this is defined it will automatically be called for you this will be called automatically after the render function is triggered for the first time
+	const urlParams = new URLSearchParams(window.location.search);
 
-    if (urlParams.get('names')) {
-        this.setState({text: urlParams.get('names')}
-      );
-    }
-  }
+	if (urlParams.get('names')) {
+		this.setState({text: urlParams.get('names')}
+		);
+	}
+	}
 
-  render() {
-    //location.search.substring(6, location.search.length).split(',')
+	render() {
+	//location.search.substring(6, location.search.length).split(',')
 
-    //simply calling this.setState({text: urlParams.get('names')}); in a render function triggers the render function which sends you into a loop
+	//simply calling this.setState({text: urlParams.get('names')}); in a render function triggers the render function which sends you into a loop
 
-    return (
-      <div className="box">
-        <p>Please enter your team members names comma seperated, then hit the random genator button</p>
-          <TextArea onChange={ this._textChanged } className={'textarea'} value = { this.state.text } />
-        <br/>
-          <Button onClick={ this._buttonClicked } className={'button'}/>
-        <h2 className="result">{ this.state.selected }</h2>
-      </div>
-    );
-  }
+	return (
+		<div className="box">
+		<p>Please enter your team members names comma seperated, then hit the random genator button</p>
+			<TextArea onChange={ this._textChanged } className={'textarea'} value = { this.state.text } />
+		<br/>
+			<Button onClick={ this._buttonClicked } className={'button'}/>
+		<h2 className="result">{ this.state.selected }</h2>
+		</div>
+	);
+	}
 
-  _textChanged(event) {
-    event.preventDefault();
-    // this._updateUrlQueryString();
-    // window.location.search = '?names=' + this.state.text;
-    // this.text = event.target.value;//(regular variable nothing is happening) this method gives it a new value when targetted
-    this.setState({text: event.target.value}) //once the state(variable) is changed rerender happens */
-  }
-  //just doing this caused the page to refresh with every keypress
+	_textChanged(event) {
+		event.preventDefault();
+		// this._updateUrlQueryString();
+		// window.location.search = '?names=' + this.state.text;
+		// this.text = event.target.value;//(regular variable nothing is happening) this method gives it a new value when targetted
+		this.setState({text: event.target.value}) //once the state(variable) is changed rerender happens */
+	}
+	//just doing this caused the page to refresh with every keypress
 
 	_buttonClicked() {
 		let names = this.state.text.split(',');
@@ -67,9 +67,12 @@ class App extends React.Component {// this is the root of the app
 		});
 	}
 
-  // _updateUrlQueryString() {
-  //   window.location.search = '?names=' + this.state.text;
-  // }// this causes the entire page to refresh 
+	_updateUrlQueryString() {
+		const { pathname, origin } = window.location;
+		const URL = `${ origin}${ pathname }?names=${ this.state.text }`;
+
+		window.history.replaceState({} , '', URL);
+	}
 }
 
 export default App;
